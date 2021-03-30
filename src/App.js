a412d54fb0b77f641a79c17a50008a67c07bf5fc
capriciousbased <mdanisulhoquekhan@gmail.com>
2021-03-30 13:33:04 +0200

    updated navbar

--- src/App.js
+++ src/App.js
@@ -2,9 +2,10 @@ import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 import { createMuiTheme, ThemeProvider } from "@material-ui/core";
 import { purple } from "@material-ui/core/colors";
 import Home from "./Home";
-import NavBar from "./components/NavBar";
+
 import ProductsTabBar from "./components/ProductListTab/ProductsTabBar";
 import ProductDetails from "./components/ProductsDetails";
+import NavBar from "./components/Navbar";
 
 const theme = createMuiTheme({
   palette: {
@@ -28,15 +29,9 @@ function App() {
       <Router>
         <NavBar />
         <Switch>
-          <Route exact path="/">
+          <Route>
             <Home />
           </Route>
-          <Route exact path="/productsTab">
-            <ProductsTabBar />
-          </Route>
-          <Route path="/products/:id">
-          <ProductDetails />
-        </Route>
         </Switch>
       </Router>
     </ThemeProvider>
